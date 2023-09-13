209f812f40a9d5701329b5cebfc1acfbd4862e96
Vincent Monard <vincent.monard@free.fr>
2023-09-13 23:46:43 +0200

    Definitely having fun

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './landing.module.css';
+import { useRect } from '../../hooks/use_rect.js';
+
+const ALPHABET =
+	'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+function generateRandomString(size = 10000) {
+	return new Array(size)
+		.fill(undefined)
+		.map(() => ALPHABET.charAt(Math.random() * ALPHABET.length))
+		.join('');
+}
 
 export const Landing = () => {
+	const [randomString, setRandomString] = useState(generateRandomString);
+
+	const [pointerX, setPointerX] = useState(0);
+	const [pointerY, setPointerY] = useState(0);
+	const containerRef = useRef(null);
+	const containerRect = useRect(containerRef);
+
+	function regenerateRandomString(event) {
+		setPointerX(event.clientX - containerRect.x);
+		setPointerY(event.clientY - containerRect.y);
+
+		setRandomString(generateRandomString());
+	}
+
 	return (
-		<div className={classes.landing}>
-			<img
-				className={classes.image}
-				src={'../../../assets/91s.jpg'}
-				alt={'91s'}
-			/>
+		<div className={classes.landing} onMouseMove={regenerateRandomString}>
+			<div className={classes.gradientContainer} />
+			<div
+				ref={containerRef}
+				className={classes.stringContainer}
+				style={{
+					maskImage: `radial-gradient(calc(280px * 0.8) circle at ${pointerX}px ${pointerY}px,rgb(255 255 255) 50%, rgb(255 255 255 / 25%),transparent)`
+				}}
+			>
+				{randomString}
+			</div>
 		</div>
 	);
 };
