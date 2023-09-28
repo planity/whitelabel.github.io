5fde2c27eb7bb9792867fb0c8a989240d0592bb6
Vincent Monard <vincent.monard@free.fr>
2023-09-28 21:07:04 +0200

    Seems that dev server now works without `index.html` at the end of URL

--- vite.config.js
+++ vite.config.js
@@ -8,27 +8,27 @@ export default defineConfig({
 		react(),
 		mpaPlugin({
 			pages: {
-				multi_account: {
+				'multi_account': {
 					entry: './src/components/main.jsx',
 					filename: 'multi_account.html',
 					template: '/src/pages/multi_page_template.html'
 				},
-				multi_appointment: {
+				'multi_appointment': {
 					entry: './src/components/main.jsx',
 					filename: 'multi_appointment.html',
 					template: '/src/pages/multi_page_template.html'
 				},
-				multi_gift_vouchers: {
+				'multi_gift_vouchers': {
 					entry: './src/components/main.jsx',
 					filename: 'multi_gift_vouchers.html',
 					template: '/src/pages/multi_page_template.html'
 				},
-				multi_online_shop: {
+				'multi_online_shop': {
 					entry: './src/components/main.jsx',
 					filename: 'multi_online_shop.html',
 					template: '/src/pages/multi_page_template.html'
 				},
-				index: {
+				'': {
 					entry: './src/components/main.jsx',
 					filename: 'index.html',
 					template: '/src/pages/single_page_template.html'
